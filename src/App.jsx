b511634d409f9238e41c99220cac1349ee540d1b
Adam <goldiron2103@gmail.com>
2025-06-29 23:12:30 +0700

    Update Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Beranda from './pages/Beranda';
 import Galeri from './pages/Galeri';
@@ -7,16 +7,20 @@ import Laporan from './pages/Laporan';
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <div className="p-4">
-        <Routes>
-          <Route path="/" element={<Beranda />} />
-          <Route path="/galeri" element={<Galeri />} />
-          <Route path="/profil" element={<Profil />} />
-          <Route path="/laporan" element={<Laporan />} />
-        </Routes>
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-gray-50">
+        {/* Navbar */}
+        <Navbar />
+        {/* Main Content */}
+        <main className="flex-1 pt-20 p-6 overflow-y-auto">
+          <Routes>
+            <Route path="/" element={<Beranda />} />
+            <Route path="/galeri" element={<Galeri />} />
+            <Route path="/profil" element={<Profil />} />
+            <Route path="/laporan" element={<Laporan />} />
+          </Routes>
+        </main>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
