3b7fdeaa067cbc6f6b7c3e573de5d43fb3ed3b79
Adam <goldiron2103@gmail.com>
2025-07-08 08:34:10 +0700

    Update

--- src/pages/Galeri.jsx
+++ src/pages/Galeri.jsx
@@ -1,10 +1,13 @@
 // Galeri.jsx
 
-import { ArrowUp } from "lucide-react";
+import { useState } from "react";
+import { ArrowUp, X } from "lucide-react";
 import { grupGaleri } from "../data/DataFoto";
 import Footer from "../components/Footer";
 
 export default function Galeri() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <>
       <div className="px-4 py-8 max-w-6xl mx-auto">
@@ -30,7 +33,8 @@ export default function Galeri() {
                     key={j}
                     src={src}
                     alt={`Foto ${i + 1}-${j + 1}`}
-                    className="w-full h-48 object-cover rounded-xl shadow-sm hover:shadow-md transition-shadow"
+                    onClick={() => setSelectedImage(src)}
+                    className="w-full h-48 object-cover rounded-xl shadow-sm hover:shadow-md hover:cursor-pointer transition-shadow"
                   />
                 ))}
               </div>
@@ -39,11 +43,36 @@ export default function Galeri() {
         </div>
       </div>
 
+      {/* Modal Gambar */}
+      {selectedImage && (
+        <div
+          onClick={() => setSelectedImage(null)}
+          className="fixed inset-0 bg-black/20 backdrop-blur-md bg-opacity-70 flex items-center justify-center z-50"
+        >
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="relative max-w-3xl w-full p-4"
+          >
+            <button
+              onClick={() => setSelectedImage(null)}
+              className="absolute top-2 right-2 text-white bg-red-500 rounded-full p-1 hover:bg-red-600"
+            >
+              <X />
+            </button>
+            <img
+              src={selectedImage}
+              alt="Preview"
+              className="w-full max-h-[80vh] object-contain rounded-lg shadow-xl"
+            />
+          </div>
+        </div>
+      )}
+
       <button
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-        className="mb-6 px-4 items-center py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition"
+        className="mb-6 px-4 flex items-center gap-1 py-2 bg-blue-600 text-white rounded-lg shadow-md hover:bg-blue-700 transition mx-auto"
       >
-        <ArrowUp className="" />
+        <ArrowUp className="w-4 h-4" /> Kembali ke Atas
       </button>
 
       <Footer />
