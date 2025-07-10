f1af1890fbcc4fabdfb6cd1582f5dd8907b5b91b
Adam <goldiron2103@gmail.com>
2025-07-10 07:13:44 +0700

    Update Penguatan Karir

--- src/pages/PenguatanKarir.jsx
+++ src/pages/PenguatanKarir.jsx
@@ -1,8 +1,11 @@
 export default function PenguatanKarir() {
   return (
     <>
-      <div className="px-4 py-8 max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold mb-2">Penguatan Karir</h1>
+      <div className="min-h-screen shadow-xl flex flex-col md:flex-row items-center justify-between px-15 md:px-16 py-10 bg-gradient-to-b rounded-2xl from-white to-blue-50">
+        {/* Kiri: Teks */}
+        <div className="md:w-1/2 text-center md:text-left">
+          <h1 className="text-3xl font-bold mb-2">Penguatan Karir</h1>
+        </div>
       </div>
     </>
   );
