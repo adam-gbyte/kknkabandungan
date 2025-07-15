fd17597dfa1b11f1953117c2a2d38b3678faa4cb
Adam <goldiron2103@gmail.com>
2025-07-15 22:42:23 +0700

    Update Pemantapan Karir

--- src/components/form/PreferensiKarir.jsx
+++ src/components/form/PreferensiKarir.jsx
@@ -1,4 +1,5 @@
 import CheckboxGroup from "../CheckboxGroup";
+import Dropdown from "../Dropdown";
 
 export default function PreferensiKarir({
   karir,
@@ -9,23 +10,33 @@ export default function PreferensiKarir({
   setLulus = () => {},
 }) {
   return (
-    <CheckboxGroup
-      label="Bidang Karier"
-      soal="Bidang karier apa yang menurutmu paling menarik?"
-      selectedOptions={karir}
-      onChange={setKarir}
-      options={[
-        "Teknologi/IT",
-        "Desain/Kreatif",
-        "Pendidikan",
-        "Psikologi/Sosial",
-        "Kesehatan",
-        "Bisnis/Keuangan",
-        "Masih Bingung",
-      ]}
-      showLainnya={true}
-      valueLainnya={karirLainnya}
-      onChangeLainnya={setKarirLainnya}
-    />
+    <>
+      <CheckboxGroup
+        label="Bidang Karier"
+        soal="Bidang karier apa yang menurutmu paling menarik?"
+        selectedOptions={karir}
+        onChange={setKarir}
+        options={[
+          "Teknologi/IT",
+          "Desain/Kreatif",
+          "Pendidikan",
+          "Psikologi/Sosial",
+          "Kesehatan",
+          "Bisnis/Keuangan",
+          "Masih Bingung",
+        ]}
+        showLainnya={true}
+        valueLainnya={karirLainnya}
+        onChangeLainnya={setKarirLainnya}
+      />
+      <Dropdown
+        label="Setelah lulus..."
+        soal="Setelah lulus nanti, kamu ingin?"
+        value={lulus}
+        onChange={setLulus}
+        options={["Langsung Bekerja", "Malnjutkan Kuliah", "Belum Tahu"]}
+        showLainnya={false}
+      />
+    </>
   );
 }
