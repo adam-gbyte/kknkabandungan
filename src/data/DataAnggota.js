6805d219e8b67693651f8b32d90841e0fff56351
Adam <goldiron2103@gmail.com>
2025-07-21 12:40:38 +0700

    Update anggotaKkn data with new names and photos

--- src/data/DataAnggota.js
+++ src/data/DataAnggota.js
@@ -1,98 +1,103 @@
 export const anggotaKkn = {
   ketua: [
     {
-      nama: "Nama Ketua",
-      foto: "https://ik.imagekit.io/goldiron/Ketua/IMG_2600.JPG?updatedAt=1751459018564",
+      nama: "Ketua",
+      foto: "https://ik.imagekit.io/goldiron/KETUA/SnapInsta.to_514667742_17846669991514649_1794136730361000589_n.jpg?updatedAt=1753076490404",
       jabatan: "Ketua KKN",
     },
   ],
-  dpl: [{ nama: "Dosen Pembimbing", foto: "/images/dpl.jpg" }],
+  dpl: [
+    {
+      nama: "Dosen Pembimbing",
+      foto: "https://ik.imagekit.io/goldiron/DPL/SnapInsta.to_515903200_17847464862514649_6455523048777979884_n.jpg?updatedAt=1753076455663",
+    },
+  ],
   sekretaris: [
     {
-      nama: "Nama Sekretaris",
-      foto: "https://ik.imagekit.io/goldiron/SEKRETARIS/IMG_2588.JPG?updatedAt=1751459021949",
+      nama: "Sekretaris",
+      foto: "https://ik.imagekit.io/goldiron/SEKRETARIS/SnapInsta.to_513980621_17846668650514649_6637614258736059356_n.jpg?updatedAt=1753076416462",
     },
   ],
   bendahara: [
     {
-      nama: "Nama Bendahara",
-      foto: "https://ik.imagekit.io/goldiron/BENDAHARA/Bendahara.png?updatedAt=1751459005578",
+      nama: "Bendahara",
+      foto: "https://ik.imagekit.io/goldiron/BENDAHARA/SnapInsta.to_514491165_17846665632514649_8975703587579750573_n.jpg?updatedAt=1753076326586",
     },
   ],
   pdd: [
     {
       nama: "Anggota PDD 1",
-      foto: "https://ik.imagekit.io/goldiron/PDD/IMG_2577.JPG?updatedAt=1751459019237",
+      foto: "https://ik.imagekit.io/goldiron/PDD/SnapInsta.to_514482410_17846661663514649_8827109122488370901_n.jpg?updatedAt=1753075506656",
     },
     {
       nama: "Anggota PDD 2",
-      foto: "https://ik.imagekit.io/goldiron/PDD/IMG_2576.JPG?updatedAt=1751459019242",
+      foto: "https://ik.imagekit.io/goldiron/PDD/SnapInsta.to_514222495_17846661672514649_2702662847694403050_n.jpg?updatedAt=1753075506317",
     },
     {
       nama: "Anggota PDD 3",
-      foto: "https://ik.imagekit.io/goldiron/PDD/IMG_2578.JPG?updatedAt=1751459019409",
+      foto: "https://ik.imagekit.io/goldiron/PDD/SnapInsta.to_515319462_17846661690514649_8732633772650108855_n.jpg?updatedAt=1753075506521",
     },
     {
       nama: "Anggota PDD 4",
-      foto: "https://ik.imagekit.io/goldiron/PDD/IMG_2579.JPG?updatedAt=1751459021956",
+      foto: "https://ik.imagekit.io/goldiron/PDD/SnapInsta.to_504819472_17846661681514649_3599822434744754059_n.jpg?updatedAt=1753075506389",
     },
   ],
   logistik: [
     {
       nama: "Anggota Logistik 1",
-      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/IMG_2604.JPG?updatedAt=1751459019408",
+      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/SnapInsta.to_515270660_17846663718514649_9081287808761902434_n.jpg?updatedAt=1753075671079",
     },
     {
       nama: "Anggota Logistik 2",
-      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/IMG_2585.JPG?updatedAt=1751459018070",
+      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/SnapInsta.to_514731527_17846663736514649_9150754432298477339_n.jpg?updatedAt=1753075671083",
     },
     {
       nama: "Anggota Logistik 3",
-      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/IMG_2586.JPG?updatedAt=1751459016964",
+      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/SnapInsta.to_514872125_17846663745514649_2213478648889625437_n.jpg?updatedAt=1753075671311",
     },
     {
       nama: "Anggota Logistik 4",
-      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/IMG_2587.JPG?updatedAt=1751459018188",
+      foto: "https://ik.imagekit.io/goldiron/LOGISTIK/SnapInsta.to_515307649_17846663727514649_4049587769303174509_n.jpg?updatedAt=1753075671098",
     },
   ],
   humas: [
     {
       nama: "Anggota Humas 1",
-      foto: "https://ik.imagekit.io/goldiron/HUMAS/IMG_2583.JPG?updatedAt=1751459011945",
+      foto: "https://ik.imagekit.io/goldiron/HUMAS/SnapInsta.to_515107650_17846664810514649_2329282857628286108_n.jpg?updatedAt=1753075770816",
     },
     {
       nama: "Anggota Humas 2",
-      foto: "https://ik.imagekit.io/goldiron/HUMAS/IMG_2580.JPG?updatedAt=1751459004734",
+      foto: "https://ik.imagekit.io/goldiron/HUMAS/SnapInsta.to_514876998_17846664825514649_3722382329199974533_n.jpg?updatedAt=1753075770825",
     },
     {
       nama: "Anggota Humas 3",
-      foto: "https://ik.imagekit.io/goldiron/HUMAS/IMG_2581.JPG?updatedAt=1751459004595",
+      foto: "https://ik.imagekit.io/goldiron/HUMAS/SnapInsta.to_515079156_17846664843514649_1103946581192839623_n.jpg?updatedAt=1753075770975",
     },
     {
       nama: "Anggota Humas 4",
-      foto: "https://ik.imagekit.io/goldiron/HUMAS/IMG_2582.JPG?updatedAt=1751459005964",
+      foto: "https://ik.imagekit.io/goldiron/HUMAS/SnapInsta.to_514478404_17846664834514649_8126761009291184827_n.jpg?updatedAt=1753075771001",
     },
   ],
   acara: [
     {
       nama: "Anggota Acara 1",
-      foto: "https://ik.imagekit.io/goldiron/ACARA/IMG_2573.JPG?updatedAt=1751459005630",
+      foto: "https://ik.imagekit.io/goldiron/ACARA/SnapInsta.to_514653282_17846666859514649_8195149835868945700_n.jpg?updatedAt=1753076131192",
     },
     {
       nama: "Anggota Acara 2",
-      foto: "https://ik.imagekit.io/goldiron/ACARA/IMG_2572.JPG?updatedAt=1751459004645",
+      foto: "https://ik.imagekit.io/goldiron/ACARA/SnapInsta.to_514757421_17846666820514649_736710181609953863_n.jpg?updatedAt=1753076131146",
     },
     {
       nama: "Anggota Acara 3",
-      foto: "https://ik.imagekit.io/goldiron/ACARA/IMG_2575.JPG?updatedAt=1751459005670",
+      foto: "https://ik.imagekit.io/goldiron/ACARA/SnapInsta.to_515301976_17846666811514649_394224183977242724_n.jpg?updatedAt=1753076131178",
     },
     {
       nama: "Anggota Acara 4",
-      foto: "https://ik.imagekit.io/goldiron/ACARA/IMG_2574.JPG?updatedAt=1751459005765",
+      foto: "https://ik.imagekit.io/goldiron/ACARA/SnapInsta.to_515074783_17846666841514649_8444693919278991763_n.jpg?updatedAt=1753076131181",
     },
     {
       nama: "Anggota Acara 5",
-      foto: "https://ik.imagekit.io/goldiron/ACARA/IMG_2571.JPG?updatedAt=1751459004669",
+      foto: "https://ik.imagekit.io/goldiron/ACARA/SnapInsta.to_514100240_17846666799514649_5999386340725142014_n.jpg?updatedAt=1753076131157",
     },
   ],
 };
